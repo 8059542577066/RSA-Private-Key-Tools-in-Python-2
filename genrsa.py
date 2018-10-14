b246703f33bbad6a54cd1865ee42d5bdb93d2339
Minjun (Dominic) Jung <m48497943@gmail.com>
2018-10-14 14:20:16 +1100

    Add files via upload

--- genrsa.py
+++ genrsa.py
@@ -4,6 +4,9 @@ import textwrap
 import os
 
 
+PKCS8_RSA_STRING = "020100300D06092A864886F70D0101010500"
+
+
 def isProbablePrime(n):
     return pow(random.getrandbits(64), n - 1, n) == 1
 
@@ -20,30 +23,33 @@ def getPrivateExponent(p, q, e):
         k += 1
     return (k * (p - 1) * (q - 1) + 1) / e
 
+def appendSizePrefix(b):
+    size = len(b) / 2
+    if size <= 0x7F:
+        b = format(size, "x") + b
+        b = ("0" if len(b) % 2 == 1 else "") + b
+    elif size <= 0xFF:
+        b = "81" + format(size, "x") + b
+    elif size <= 0xFFFF:
+        b = format(size, "x") + b
+        b = "82" + ("0" if len(b) % 2 == 1 else "") + b
+    elif size <= 0xFFFFFF:
+        b = format(size, "x") + b
+        b = "83" + ("0" if len(b) % 2 == 1 else "") + b
+    else:
+        b = format(size, "x") + b
+        b = "84" + ("0" if len(b) % 2 == 1 else "") + b
+    return b
+
 def longToASN1Int(n):
     n = format(n, "x")
     if len(n) % 2 == 1:
         n = "0" + n
     if n[0] not in [str(i) for i in xrange(8)]:
         n = "00" + n
-    size = len(n) / 2
-    if size <= 0x7F:
-        n = format(size, "x") + n
-        n = ("0" if len(n) % 2 == 1 else "") + n
-    elif size <= 0xFF:
-        n = "81" + format(size, "x") + n
-    elif size <= 0xFFFF:
-        n = format(size, "x") + n
-        n = "82" + ("0" if len(n) % 2 == 1 else "") + n
-    elif size <= 0xFFFFFF:
-        n = format(size, "x") + n
-        n = "83" + ("0" if len(n) % 2 == 1 else "") + n
-    else:
-        n = format(size, "x") + n
-        n = "84" + ("0" if len(n) % 2 == 1 else "") + n
-    return "02" + n
+    return "02" + appendSizePrefix(n)
 
-def getSequence(n, e, d, p, q, dP, dQ, qInv):
+def getPKCS1Sequence(n, e, d, p, q, dP, dQ, qInv):
     v = longToASN1Int(0)
     n = longToASN1Int(n)
     e = longToASN1Int(e)
@@ -53,23 +59,14 @@ def getSequence(n, e, d, p, q, dP, dQ, qInv):
     dP = longToASN1Int(dP)
     dQ = longToASN1Int(dQ)
     qInv = longToASN1Int(qInv)
-    r = v + n + e + d + p + q + dP + dQ + qInv
-    size = len(r) / 2
-    if size <= 0x7F:
-        r = format(size, "x") + r
-        r = ("0" if len(r) % 2 == 1 else "") + r
-    elif size <= 0xFF:
-        r = "81" + format(size, "x") + r
-    elif size <= 0xFFFF:
-        r = format(size, "x") + r
-        r = "82" + ("0" if len(r) % 2 == 1 else "") + r
-    elif size <= 0xFFFFFF:
-        r = format(size, "x") + r
-        r = "83" + ("0" if len(r) % 2 == 1 else "") + r
-    else:
-        r = format(size, "x") + r
-        r = "84" + ("0" if len(r) % 2 == 1 else "") + r
-    return "30" + r
+    s = v + n + e + d + p + q + dP + dQ + qInv
+    return "30" + appendSizePrefix(s)
+
+def getPKCS1Octet(b):
+    return "04" + appendSizePrefix(b)
+
+def getSequence(b):
+    return "30" + appendSizePrefix(b)
 
 def createKey(size, fileName):
     start = time.time()
@@ -87,12 +84,13 @@ def createKey(size, fileName):
         dP = d % (p - 1)
         dQ = d % (q - 1)
         qInv = pow(q, p - 2, p)
-        seq = getSequence(n, e, d, p, q, dP, dQ, qInv)
+        seq = getPKCS1Sequence(n, e, d, p, q, dP, dQ, qInv)
+        seq = getSequence(PKCS8_RSA_STRING + getPKCS1Octet(seq))
         base64 = "".join(seq.decode("hex").encode("base64").split())
         with open(fileName, "w") as file:
-            file.write("-----BEGIN RSA PRIVATE KEY-----\n")
+            file.write("-----BEGIN PRIVATE KEY-----\n")
             file.write(textwrap.fill(base64, 64))
-            file.write("\n-----END RSA PRIVATE KEY-----\n")
+            file.write("\n-----END PRIVATE KEY-----\n")
         finish = time.time()
         print "Time Taken: " + str(round(finish - start, 3)) + " Seconds"
     else:
@@ -100,9 +98,10 @@ def createKey(size, fileName):
         createKey(size, fileName)
 
 
-size = int(raw_input("Enter RSA Key Size: "))
-amount = int(raw_input("Enter Number of Key Pairs to Create: "))
-for i in xrange(amount):
-    print "\n" + str(i + 1) + "."
-    createKey(size, "RSA PRIVATE KEY " + str(i + 1) + ".key")
-os.system("pause")
+if __name__ == "__main__":
+    size = int(raw_input("Enter RSA Key Size: "))
+    amount = int(raw_input("Enter Number of Key Pairs to Create: "))
+    for i in xrange(amount):
+        print "\n" + str(i + 1) + "."
+        createKey(size, "RSA PRIVATE KEY " + str(i + 1) + ".key")
+    os.system("pause")
